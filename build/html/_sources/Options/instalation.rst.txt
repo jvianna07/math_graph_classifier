7042c4787d86cb709c00284d5ef42a5ebb1da83b
Joaquim Viana <jviana@unilurio.ac.mz>
2025-07-18 17:07:25 -0300

    added colored error message in setup.sh

--- build/html/_sources/Options/instalation.rst.txt
+++ build/html/_sources/Options/instalation.rst.txt
@@ -6,22 +6,42 @@ Clonar o repositório
 ======================
 
 Clone o repositório na sua máquina.
-Para clonar, execute o código::    
+Para clonar e entrar na pasta, execute o código:
+::    
     git clone git@github.com:jvianna07/math_graph_classifier.git && cd math_graph_classifier/
 
 
 Criar ambiente virtual e instalar as dependências
 ===================================================
-Crie o ambiente virtual e instale as dependências. Você pode criar o ambiente virtual e dar o nome a sua escolha.
-Para instalar as dependências execute::
+Crie o ambiente virtual e instale as dependências.
+Você pode criar o ambiente virtual e dar o nome a sua escolha.
+No entanto, esta aplicação funciona com python 3.9 a 3.12 que é compatível com o tensorflow 2
+caso use ``conda`` use o comando ``create`` para criar o ambiente virtual.
+::
+    conda create -n fgraphs python=3.10
+
+Este comando irá criar o ambiente virtual com o nome ``fgraphs`` e python 3.10.
+Para ativar use o comando ``activate``.
+::
+    conda activate fgraphs
+
+
+Para instalar as dependências execute:
+::
     pip install -r requirements.txt
 
 
-Obter dataset
-===============
+Obter o dataset (opcional)
+=======================================================
 
 Após instalar, você pode baixar o dataset de teste para classificar.
-Para obter o dataset, execute::
+Para obter o dataset, execute:
+::
     ./setup
+
+
+Alternativamente pode usar o comando ``source`` para o mesmo fim.
+::
+    source setup.sh
 
 Isto irá criar uma pasta com o nome dataset e um arquivo de dataset nela.
--- setup.sh
+++ setup.sh
@@ -2,25 +2,45 @@
 
 #Link do arquivo: https://drive.google.com/file/d/1SqDrEWdYIKbx71ycqHDi81R7l6trl26V/view?usp=sharing
 
+dataset_OK="Dataset baixado com sucesso!"
+dataset_error="Erro ao baixar o dataset!"
+
+
+# Cores para terminal
+RED='\033[0;31m' #cor vermelha
+RESET='\033[0m' #restaura para cor original
+
+# Função para checar status do último comando
+# Uso: check_status "Mensagem de sucesso" "Mensagem de erro"
+check_status() {
+    if [[ $? == 0 ]] 
+    then
+        echo "$1 "
+    else
+        echo -e "${RED}$2 ${RESET}"
+    fi
+}
+
 # ID do arquivo no Google Drive
-file_id="1SqDrEWdYIKbx71ycqHDi81R7l6trl26V"
+file_id="-1SqDrEWdYIKbx71ycqHDi81R7l6trl26V"
 
 # Baixa o arquivo com gdown
 echo "Baixando dataset.pkl.zip com gdown..."
-gdown --id "$file_id" --output "dataset.pkl.zip"
+gdown "$file_id" --output "dataset.pkl.zip"
+check_status "Dataset baixado com sucesso" "Erro ao baixar do dataset."
 
-echo "Download concluído: dataset.pkl.zip"
-
-echo "Unziping file..."
-
+# Descompactar o arquivo 
+echo "Descompactando o dataset..."
 unzip -o dataset.pkl.zip
+check_status "Dataset descompactado com sucesso!" "Erro ao descompactar o dataset."
 
-echo "Removendo ficheiros duplicados"
-
+# Remover Arquivos duplicados 
+echo "Removendo arquivos duplicados..."
 rm dataset.pkl.zip
+check_status "Arquivos duplicados foram removidos!" "Erro ao remover arquivos duplicados."
 
-echo "Criando o dataset"
-
+# Criando o dataset 
+echo "Criando o dataset..."
 mkdir -p dataset && mv -f LID_fg_dataset.pkl dataset/dataset.pkl
+check_status "Dataset criado com sucesso!" "Erro ao criar o dataset."
 
-echo "Finalizado."
